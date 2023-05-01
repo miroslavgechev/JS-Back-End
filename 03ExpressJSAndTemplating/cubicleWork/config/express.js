f51d1fabb1a2b2f223352bda55da781c27f9fee6
Miroslav Gechev <vindaloo89@gmail.com>
2023-05-01 17:02:36 +0300

    ExpressJS and Templating Tasks

---
+++ 03ExpressJSAndTemplating/cubicleWork/config/express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+
+    //Setup the view engine
+    app.set('view engine', 'hbs');
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs'
+    }))
+
+    //Setup the body parser
+    app.use(bodyParser.urlencoded({extended: false}))
+
+    //Setup the static files
+    app.use(express.static('static'))
+};
---
+++ 03ExpressJSAndTemplating/cubicleWork/config/routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const cubes = require('./database.json');
+const pageController = require('../controllers/cubesController');
+
+
+module.exports = (app) => {
+    app.use(router);
+
+    app.get('/', pageController.getHomePage)
+
+    app.get('/about', pageController.getAboutPage)
+
+    app.get('/create', pageController.getCreatePage)
+    app.post('/create', pageController.postCreatePage)
+
+    app.get('/details/:id', pageController.getDetailsPage)
+
+    app.get('*', pageController.getErrorPage)
+};
